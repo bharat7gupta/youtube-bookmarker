840fb130e4f8265901693658118454965d6439cc
Bharat Gupta <bharat7gupta@Us.local>
2025-05-25 11:55:43 +0530

    update loop section

--- src/page/LoopSection.tsx
+++ src/page/LoopSection.tsx
@@ -14,14 +14,14 @@ export default function LoopSection({ loopData, duration, hidden }: LoopSectionP
 
   const leftPosition = ((loopData.startTime / duration) * 100);
   const rightPosition = (100 - ((loopData.endTime / duration) * 100));
-  const className = `ct-bookmarks-loop${hidden ? ' ct-hide-bookmark' : ''}`;
+  const className = `ct-bookmarks-loop ${hidden ? 'ct-hide-bookmark' : ''}`;
   
   return (
     <div 
       className={className}
       style={{ 
-        left: `${leftPosition}%`,
-        right: `${rightPosition}%`
+        left: `calc(${leftPosition}% + 2px)`,
+        right: `calc(${rightPosition}% + 2px)`
       }}
     />
   );
